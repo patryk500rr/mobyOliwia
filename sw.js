66e91dc75934c40c8b7df3a0a8ac6ae57746a9f0
Patryk Nowak <patryknowakmc@gmail.com>
2025-03-01 15:21:21 +0100

    pwa

--- sw.js
+++ sw.js
@@ -64,3 +64,19 @@ self.addEventListener('activate', (event) => {
         })
     );
 });
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
