2708657a9a826ccb47e5feb17af92ea05eedb106
Patryk Nowak <patryknowakmc@gmail.com>
2025-03-01 15:14:40 +0100

    sw

--- sw.js
+++ sw.js
@@ -50,3 +50,17 @@ self.addEventListener('fetch', (event) => {
         })
     );
 });
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== 'pwa-cache') {
+                        console.log('Usuwanie starej pamięci podręcznej:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
